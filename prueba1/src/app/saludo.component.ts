4467206420c0aaa4bf641519861630236d106a67
teresa-carmona <31965190+teresa-carmona@users.noreply.github.com>
2018-10-29 13:29:31 +0100

    Initial commit

---
+++ prueba1/src/app/saludo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'saludo',
+  template: 'Good morning Vietnam!',
+  styleUrls: ['./app.component.css']
+})
+export class SaludoComponent {
+  title = 'saludo';
+}
